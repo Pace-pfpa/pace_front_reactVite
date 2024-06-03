fd322d6fbc03481ce9c296faf5947712133da398
beamstriz <anamtsoares2003@gmail.com>
2024-06-03 15:26:46 -0300

    implementando a tabela pautistas e quantidade de audiencias na pagina home

---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+export const baseURL = 'http://localhost:8080/';
+export const baseVisaoURL = 'http://localhost:3002';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Typography, Grid, Card, CardContent, CardHeader } from '@mui/material';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
@@ -17,6 +17,7 @@ import logo from '../assets/pace.png';
 import logodopace from '../assets/logodopace.png';
 import Button from '@mui/material/Button';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import { baseURL } from '../config/index';
 
 
 const drawerWidth = 240;
@@ -47,114 +48,121 @@ const download = () => {
 
 const TabelaPautista = () => {
   const [procuradores, setProcuradores] = useState([]);
-  React.useEffect(() => {
+  const [maxElements, setMaxElements] = useState(0);
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(baseURL + 'pauta/total');
+        const response = await fetch(baseURL + 'pauta/total', { method: 'GET' });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const maxElements = response.headers.get('maxElements');
+        setMaxElements(maxElements);
         const data = await response.json();
         setProcuradores(data);
       } catch (error) {
-        console.error(error);
+        console.error('Error fetching data:', error);
       }
     };
     fetchData();
   }, []);
 
-
-export const Home = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#517085' }}>
-        <Toolbar>
-          <img src={logodopace} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
-          <img src={logo} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
-          <Typography variant="h6" noWrap component="div">
-            - Programa de Agendamento e Controle de Escalas
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', backgroundColor: '#'},
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            {['Home'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['Cadastro', 'Escala', 'Multirão','Consulta'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-
-
-
-        <Typography paragraph>
-          <Button variant="contained" onClick={download} startIcon={<CloudDownloadIcon />}> Download Relatório </Button>
-          <br/>
-          Relatório
-          
-          <Grid container spacing={3}>
-            <CustomCard title="Varas" value="35" color="primary" />
-            <CustomCard title="Mutirões" value="0" color="success" />
-            <CustomCard title="Pautista" value="0" color="info" />
-          </Grid>
-
-            <div>
-              <Table bordered>
-                <thead className="table-success">
-                  <tr>
-                    <th style={{ paddingRight: '20px' }}>PAUTISTAS</th>
-                    <th>QUANTIDADE DE AUDIÊNCIAS</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>TODOS</td>
-                    <td>{procuradores.length > 0 && procuradores[0].saldo}</td>
-                  </tr>
-                  {procuradores.map((procurador, index) => (
-                    <tr key={index}>
-                      <td>{procurador.nome}</td>
-                      <td>{procurador.saldo}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-
-        </Typography>
-
-        
-      </Box>
-    </Box>
+    <table className='table'>
+      <thead id='theadPautista'>
+        <tr className='table-success'>
+          <th style={{ paddingRight: '20px' }} scope="col">PAUTISTAS</th>
+          <th scope="col">QUANTIDADE DE AUDIÊNCIAS</th>
+        </tr>
+      </thead>
+      <tbody id='tbodyPautista'>
+        <tr>
+          <td>TODOS</td>
+          <td>{maxElements}</td>
+        </tr>
+        {procuradores.map((obj, i) => (
+          <tr key={i}>
+            <td>{obj.nome}</td>
+            <td>{obj.quantidade}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
-}
+};
+
+  export const Home = () => {
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#517085' }}>
+          <Toolbar>
+            <img src={logodopace} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
+            <img src={logo} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
+            <Typography variant="h6" noWrap component="div">
+              - Programa de Agendamento e Controle de Escalas
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', backgroundColor: '#'},
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              {['Home'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['Cadastro', 'Escala', 'Multirão','Consulta'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+  
+          <Typography paragraph>
+            <Button variant="contained" onClick={download} startIcon={<CloudDownloadIcon />}> Download Relatório </Button>
+            <br/>
+            Relatório
+            
+            <Grid container spacing={3}>
+              <CustomCard title="Varas" value="35" color="primary" />
+              <CustomCard title="Mutirões" value="0" color="success" />
+              <CustomCard title="Pautista" value="0" color="info" />
+            </Grid>
+
+            <TabelaPautista />
+            
+          </Typography>
+          
+        </Box>
+      </Box>
+    );
+  }
+  
 
