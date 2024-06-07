5e823b5d0f3b1a71df0b912c74ef7b1707315777
beamstriz <anamtsoares2003@gmail.com>
2024-06-07 13:44:54 -0300

    ajustes no sidebar, implementação de icones e subcategorias do menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from '../Style/HomeStyle.module.css'
-import { useState, useEffect } from 'react';
-import { Box, Typography, Grid } from '@mui/material';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, Collapse, Divider, Toolbar, ListItemText, Typography, Grid, Button, CssBaseline, AppBar, Drawer } from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import logo from '../assets/pace.png';
 import logodopace from '../assets/logodopace.png';
-import Button from '@mui/material/Button';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 import { TabelaPautistaContainer } from '../services/TabelaPautistaContainer';
 import { CustomCard } from '../components/CustomCard';
-import { getMutiraoCount } from '../services/CustomCard';
-import { getPautistaCount } from '../services/CustomCard';
+import { getMutiraoCount, getPautistaCount } from '../services/CustomCard';
+import HomeIcon from '@mui/icons-material/Home';
+import AddIcon from '@mui/icons-material/Add';
+import TableIcon from '@mui/icons-material/BackupTable';
+import GroupIcon from '@mui/icons-material/Group';
+import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 
+const iconMapping = {
+  Home: <HomeIcon />,
+  Cadastro: <AddIcon />,
+  Escala: <TableIcon />,
+  Mutirão: <GroupIcon />,
+  Consulta: <SearchIcon />
+}
 
 const drawerWidth = 240;
 
@@ -38,6 +39,13 @@ const download = () => {
   export const Home = () => {
     const [mutiraoCount, setMutiraoCount] = useState(0)
     const [pautistaCount, setPautistaCount] = useState(0)
+    const [openItems, setOpenItems] = useState({
+      Cadastro: false,
+      Escala: false,
+      Mutirão: false,
+      Consulta: false
+    });
+    const [activeItem, setActiveItem] = useState('Home');
 
     useEffect(() => {
       const fetchCounts = async () => {
@@ -55,75 +63,136 @@ const download = () => {
       fetchCounts();
     }, []);
 
+    const exibirSubItens = (item) => {
+    setOpenItems((prevOpenItems) => ({
+      ...prevOpenItems,
+      [item]: !prevOpenItems[item],
+    }));
+  };
+
     return (
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#517085' }}>
-          <Toolbar>
-            <img src={logodopace} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
-            <img src={logo} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }}></img>
-            <Typography variant="h6" noWrap component="div">
-              - Programa de Agendamento e Controle de Escalas
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', backgroundColor: '#'},
-          }}
-        >
-          <Toolbar />
-          <Box sx={{ overflow: 'auto' }}>
-            <List>
-              {['Home'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <List>
-              {['Cadastro', 'Escala', 'Multirão','Consulta'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
-        </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Toolbar sx={{ mb: 2 }}/>
-  
-          <Typography paragraph variant='h6' component="div" sx={{ fontSize: '1.6rem', mb: 3 }}>
-            Relatório
-            <Button sx={{ mb: 3, marginLeft: '20px' }} variant="contained" onClick={download} startIcon={<CloudDownloadIcon />}> Download Relatório </Button>
-            
-            <Grid container spacing={3} sx={{ mb: 3 }}>
-              <CustomCard title="Varas" value="35" color="primary" />
-              <CustomCard title="Mutirões" value={mutiraoCount} color="success" />
-              <CustomCard title="Pautista" value={pautistaCount} color="info" />
-            </Grid>
-              <br/>
-            <TabelaPautistaContainer />
-            
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#517085' }}>
+        <Toolbar>
+          <img src={logodopace} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }} />
+          <img src={logo} alt="Logo do Pace" style={{ width: '50px', height: 'auto', marginRight: '10px' }} />
+          <Typography variant="h6" noWrap component="div">
+            - Programa de Agendamento e Controle de Escalas
           </Typography>
-          
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Home'].map((text) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {iconMapping[text]}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Cadastro', 'Escala', 'Mutirão', 'Consulta'].map((text) => (
+              <React.Fragment key={text}>
+                <ListItem disablePadding>
+                  <ListItemButton sx={{ 
+                    pl: 2, 
+                    bgcolor: activeItem === text ? '#e0e0e0' : 'inherit',
+                    '&:hover': {
+                      bgcolor: activeItem !== text ? '#f0f0f0' : 'inherit',
+                    },
+                  }}
+                  selected={activeItem === text}
+                  onClick={() => exibirSubItens(text)}>
+                    <ListItemIcon>
+                      {iconMapping[text]}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                    {openItems[text] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                  </ListItemButton>
+                </ListItem>
+                {text === 'Cadastro' && (
+                  <Collapse in={openItems[text]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Pautista" />
+                      </ListItemButton>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Advogado" />
+                      </ListItemButton>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Pauta" />
+                      </ListItemButton>
+                    </List>
+                  </Collapse>
+                )}
+                {text === 'Escala' && (
+                  <Collapse in={openItems[text]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Gerar"/>
+                      </ListItemButton>
+                    </List>
+                  </Collapse>
+                )}
+                {text === 'Mutirão' && (
+                  <Collapse in={openItems[text]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItemButton sx={{ pl: 4}}>
+                        <ListItemText primary="Excluir" />
+                      </ListItemButton>
+                    </List>
+                  </Collapse>
+                )}
+                {text === 'Consulta' && (
+                  <Collapse in={openItems[text]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Pautista"/>
+                      </ListItemButton>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Advogado"/>
+                      </ListItemButton>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemText primary="Escala"/>
+                      </ListItemButton>
+                    </List>
+                  </Collapse>
+                )}
+              </React.Fragment>
+            ))}
+          </List>
         </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar sx={{ mb: 2 }} />
+        <Typography paragraph variant='h6' component="div" sx={{ fontSize: '1.6rem', mb: 3 }}>
+          Relatório
+          <Button sx={{ mb: 3, marginLeft: '20px' }} variant="contained" onClick={download} startIcon={<CloudDownloadIcon />}>Download Relatório</Button>
+          <Grid container spacing={3} sx={{ mb: 3 }}>
+            <CustomCard title="Varas" value="35" color="primary" />
+            <CustomCard title="Mutirões" value={mutiraoCount} color="success" />
+            <CustomCard title="Pautista" value={pautistaCount} color="info" />
+          </Grid>
+          <TabelaPautistaContainer />
+        </Typography>
       </Box>
-    );
-  }
+    </Box>
+  );
+};
   
 
